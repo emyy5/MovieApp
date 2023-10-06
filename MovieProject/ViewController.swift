2e87ac9fac4d50b699afbd48705ec70bfddd43b3
Github <emyy5>
2023-10-06 05:48:52 +0300

    Initial Commit

---
+++ MovieProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieProject
+//
+//  Created by Eman Khaled on 06/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
